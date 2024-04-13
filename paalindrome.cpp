6745d9f4ebf18e20c75c04288344e1fa11a1e8d0
Ramendra Raman <thegentlemonk@gmail.com>
2024-04-13 23:16:09 +0530

    is it palindrome?

---
+++ paalindrome.cpp
@@ -0,0 +1,18 @@
+/*
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x%10 == 0)) return false;
+        int y = 0;
+        while (x > y){
+    	    y = y*10 + x%10;
+    	    x = x/10;
+        }
+        return (x==y || x==y/10); 
+    }
+};
