d8c0824b89dd237836d86b994db2c20e459f9b56
Ramendra Raman <thegentlemonk@gmail.com>
2024-04-27 23:57:49 +0530

    check if the number is sum of powers of 3.

---
+++ power of 3.cpp
@@ -0,0 +1,20 @@
+//check if the number is sum of powers of 3. 
+#include <iostream>
+using namespace std;
+#include<math.h>  
+int main()
+{
+
+    int b = 81;
+    int a = 3;
+    // computing power
+    double p = log10(b) / log10(a);
+    // checking to see if power is an integer or not
+    if (p - (int)p == 0) {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
